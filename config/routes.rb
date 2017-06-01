770a2c15c42dfdfe9a646574640a0f20c3389c68
lisawilliams <lisa@placeblogger.com>
2017-06-01 15:34:29 -0400

    Added routes, controllers etc for recipes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 Rails.application.routes.draw do
+  resources :recipes, except: [:new, :edit]
   resources :ingredients, only: [:index]
   post '/sign-up' => 'users#signup'
   post '/sign-in' => 'users#signin'
